37c6f951b74f14e94440eb2eadfb54899132f96d
Alan-HWbook16s <2424233600@qq.com>
2022-08-03 21:00:57 +0800

    完成两题

--- src/main.cpp
+++ src/main.cpp
@@ -11,34 +11,9 @@
 #include "AboutTree.hpp"
 using namespace std;
 
-// 148. 排序链表
-class Solution
-{
-public:
-    vector<int> temp;
-    ListNode *sortList(ListNode *head)
-    {
-        vector<int> temp;
-        ListNode *t = head;
-        while (t)
-        {
-            temp.push_back(t->val);
-            t = t->next;
-        }
-        sort(temp.begin(),temp.end());
-        t = head;
-        for (auto i = temp.begin(); i != temp.end(); i++)
-        {
-            t->val = *i;
-            t = t->next;
-        }
-        return head;
-    }
-};
-
 int main()
 {
     cout << "---Solution Call---" << endl;
-    Solution test;
+    //Solution test;
     return 0;
 }
