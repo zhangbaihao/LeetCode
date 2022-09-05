def017e0bd51019218ebb5a2ebec772ec3e8f717
Alan-Company <2424233600@qq.com>
2022-09-05 16:17:39 +0800

    整理头文件

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,27 @@
 ﻿#include <iostream>
+#include "AboutAnOperation.hpp"
+#include "AboutBack.hpp"
+#include "AboutChart.hpp"
+#include "AboutCheckAndSet.hpp"
+#include "AboutDynamicProgramming.hpp"
+#include "AboutGreedy.hpp"
+#include "AboutHard.hpp"
+#include "AboutHash.hpp"
+#include "AboutHeap.hpp"
 #include "AboutList.hpp"
 #include "AboutOther.hpp"
+#include "AboutQueueAndStack.hpp"
 #include "AboutSearch.hpp"
-#include "AboutDynamicProgramming.hpp"
-#include "AboutBack.hpp"
+#include "AboutSimulation.hpp"
+#include "AboutSlidingWindow.hpp"
 #include "AboutSimulation.hpp"
 #include "AboutSort.hpp"
-#include "AboutQueueAndStack.hpp"
-#include "AboutGreedy.hpp"
+#include "AboutStack.hpp"
 #include "AboutTree.hpp"
-//今天周六计划
-//完成
-// 538. 把二叉搜索树转换为累加树
-class LT538Solution
-{
-public:
-    int sum = 0;
-    TreeNode *convertBST(TreeNode *root)
-    {
-        if(!root){
-            return nullptr;
-        }
-        convertBST(root->right);
-        sum += root->val;
-        root->val = sum;
-        convertBST(root->left);
-        return root;
-    }
-};
-
-
 
 
 int main()
 {
     cout << "---Solution Call---" << endl;
-
     return 0;
 }
